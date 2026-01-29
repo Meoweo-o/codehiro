d8b63746de84302093cfd27712c2a159fc5df640
Meoweo-o <les2soeurdecoeur@gmail.com>
2026-01-29 22:40:44 +0100

    Update codeeden.py

--- codeeden.py
+++ codeeden.py
@@ -5,7 +5,7 @@ import os
 import discord
 from discord.ext import commands
 
-TOKEN = os.environ["DISCORD_TOKEN"]
+TOKEN = os.environ["TOKEN"]
 
 intents = discord.Intents.default()
 intents.message_content=True
